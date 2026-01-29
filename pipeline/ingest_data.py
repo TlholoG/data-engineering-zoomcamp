9c644be1265072bd72de132a3db18c716dfd604b
Tlhologelo <133032787+TlholoG@users.noreply.github.com>
2026-01-29 02:53:58 +0000

    Fix Postgres 18 volume mount in docker-compose

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,7 +4,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
-
+import click
+    
 dtype = {
     "VendorID": "Int64",
     "passenger_count": "Int64",
@@ -29,25 +30,25 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', default=2021, type=int, help='Year of data to ingest')
+@click.option('--month', default=1, type=int, help='Month of data to ingest')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
 
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize=100000
-
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month, chunksize):
+   
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+    
+
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     df_iter = pd.read_csv(
         url,
@@ -55,7 +56,7 @@ def run():
         parse_dates=parse_dates,
         iterator=True,
         chunksize=chunksize
-)
+    )
 
     first = True
 
@@ -77,6 +78,4 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-
 
