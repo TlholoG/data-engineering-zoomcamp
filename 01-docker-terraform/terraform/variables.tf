84f24c3241e1abbfb40695e05690e910b61d0c1b
Tlhologelo <133032787+TlholoG@users.noreply.github.com>
2026-02-11 09:47:10 +0000

    reordered project folders

---
+++ 01-docker-terraform/terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "project" {
+  description = "Project"
+  default     = "terraform-demo-486320"
+}
+
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/workspaces/data-engineering-zoomcamp/terraform/keys/my-creds.json"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "africa-south1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gsc_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "terraform-demo-486320-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
