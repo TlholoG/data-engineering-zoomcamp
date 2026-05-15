dca94f8847610c1a6f688a09a9d305942ecb97a3
Tlhologelo <133032787+TlholoG@users.noreply.github.com>
2026-05-15 12:51:07 +0000

    before logs

---
+++ 02-workflow-orchestration/flows/sec-plus-practice.py
@@ -0,0 +1,83 @@
+76)[Verified]
+Select the appropriate attack and remediation from each drop-down list to label the corresponding attack with its remediation.
+
+Attack Description: An attacker sends multiple SYN packets from multiple sources.
+Target: Web server      
+Attack Identified: *Botnet, RAT, Logic Bomb, Backdoor, Virus, Spyware, Worm, Adware, Randsomware, Keylogger, Phishing.   
+Best Preventative or Remediation Action:
+   *Enable DDOS protection,
+    Patch vulnerable systems,
+    Disable vulnerable services,
+    Change the default system password
+    Update the cyptographic algorythims
+    Change the default application password
+    Implement 2FA using push notification
+    Conduct a code review
+    Implement application fuzzing
+    Implement a host-based IPS
+    Disable remote access services
+
+Attack Description: The attack establishes a connection. Which allows remote commands to be executed.
+Target: User      
+Attack Identified: Botnet, *RAT, Logic Bomb, Backdoor, Virus, Spyware, Worm, Adware, Randsomware, Keylogger, *Phishing.   
+Best Preventative or Remediation Action:
+    Enable DDOS protection,
+    Patch vulnerable systems,
+    Disable vulnerable services,
+    Change the default system password
+    Update the cyptographic algorythims
+    Change the default application password
+    Implement 2FA using push notification
+    Conduct a code review
+    Implement application fuzzing
+   *Implement a host-based IPS
+    Disable remote access services
+
+Attack Description: The attack is self propagating and compromises a SQL database using well-known credentials as it moves through the network .
+Target: Database server      
+Attack Identified: Botnet, RAT, Logic Bomb, Backdoor, Virus, Spyware, *Worm, Adware, Randsomware, Keylogger, Phishing.   
+Best Preventative or Remediation Action:
+    Enable DDOS protection,
+   *Patch vulnerable systems,
+    Disable vulnerable services,
+    Change the default system password
+    Update the cyptographic algorythims
+   *Change the default application password
+    Implement 2FA using push notification
+    Conduct a code review
+    Implement application fuzzing
+    Implement a host-based IPS
+    Disable remote access services
+
+Attack Description: The attacker uses hardware to remotely monitor a user's input activity to harvest credentials.
+Target: Executive      
+Attack Identified: Botnet, RAT, Logic Bomb, Backdoor, Virus, Spyware, Worm, Adware, Randsomware, *Keylogger, Phishing.   
+Best Preventative or Remediation Action:
+    Enable DDOS protection,
+    Patch vulnerable systems,
+    Disable vulnerable services,
+    Change the default system password
+    Update the cyptographic algorythims
+    Change the default application password
+   *Implement 2FA using push notification
+    Conduct a code review
+    Implement application fuzzing
+    Implement a host-based IPS
+    Disable remote access services
+
+Attack Description: The attacker embeds hidden acces in an internally developed application that bypasses account login.
+Target: Application      
+Attack Identified: Botnet, RAT, Logic Bomb, *Backdoor, Virus, Spyware, Worm, Adware, Randsomware, Keylogger, Phishing.   
+Best Preventative or Remediation Action:
+    Enable DDOS protection,
+    Patch vulnerable systems,
+    Disable vulnerable services,
+    Change the default system password
+    Update the cyptographic algorythims
+    Change the default application password
+    Implement 2FA using push notification
+   *Conduct a code review
+    Implement application fuzzing
+    Implement a host-based IPS
+    Disable remote access services
+
